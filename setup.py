97ebec9374aa6d01811d8c80393c1abcd32df3e8
apsz3 <apsz3@git>
2024-05-30 21:16:36 -0400

    f

--- setup.py
+++ setup.py
@@ -2,21 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='nested',
-    version='0.1',
-    description='A package for working with nested data structures',
-    author='Your Name',
-    author_email='your_email@example.com',
+    version='1.0',
     packages=find_packages(),
     install_requires=[
-        # Add any dependencies your package requires here
+        # Add any dependencies required by your project here
     ],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-    ],
+    entry_points={
+        'console_scripts': [
+            'nst=nested.main:main',
+        ],
+    },
+    author='Your Name',
+    author_email='your@email.com',
+    description='A description of your project',
+    url='https://github.com/yourusername/nested',
 )
