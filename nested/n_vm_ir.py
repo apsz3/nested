2699c780782db63a63eab3310356cedb11963238
apsz3 <apsz3@git>
2024-05-31 00:05:26 -0400

    nice

--- nested/n_vm_ir.py
+++ nested/n_vm_ir.py
@@ -1,25 +1,31 @@
 from rich import print
-class VMIR:
 
+def err(msg):
+    print(f"[red]Error: {msg}[/red]")
+
+def msg(msg):
+    print(f"[green]{msg}[/green]")
+
+class VMIR:
 # Keep these functions separate as only in interpreter mode do we want to
 # cast things for example
 
-    @staticmethod
-    def add(s, n:int):
-        if n < 2:
-            print("? (Possible nonsense) Add needs 2 or more arguments!")
-            return
-        s.append(sum([int(s.pop()) for _ in range(n)]))
+        @staticmethod
+        def add(s, n:int):
+            if n < 2:
+                err("? (Possible nonsense) Add needs 2 or more arguments!")
+                return
+            s.append(sum([int(s.pop()) for _ in range(n)]))
 
-    @staticmethod
-    def print(s, n: int):
-        try:
-            args = [str(s.pop()) for _ in range(n)]
-            print(f"> {' '.join(args)}")
+        @staticmethod
+        def print(s, n: int):
+            try:
+                args = [str(s.pop()) for _ in range(n)]
+                msg(f"> {' '.join(args)}")
 
-        except IndexError:
-            print("! Need more arguments")
+            except IndexError:
+                err("! Need more arguments")
 
-    @staticmethod
-    def list(s, n: int):
-        s.append([s.pop() for _ in range(n)])
+        @staticmethod
+        def list(s, n: int):
+            s.append([s.pop() for _ in range(n)])
